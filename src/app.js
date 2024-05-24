29330bae18d99d103e1d4f16cf38952eb879fbcc
leegilhyeon <164996803+leegilhyeon@users.noreply.github.com>
2024-05-24 17:09:31 +0900

    에러처리 미들웨어 추가

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import 'dotenv/config';
+import errorHandler from './middlewares/error-handler.middleware.js';
 
 const app = express();
 const port = process.env.SERVER_PORT;
+
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
     return res.json('hello');
